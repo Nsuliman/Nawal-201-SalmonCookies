a9e620b3a654f62cc94644e0f84a6c0e8de7aa7c
Nsuliman <nsuliman88@gmail.com>
2020-01-12 12:23:06 +0200

    JQuery Done , Form completed

--- js/cookie.js
+++ js/cookie.js
@@ -3,7 +3,7 @@
 
 let totalDailyCookiesOfLoc = [];
 let megaTotal = 0;
-
+var locationsNames = [];
 /////////////////////// Constructor Functions ///////////////
 function Location(nameoflocation, min, max, avgCookieSale) {
 
@@ -16,6 +16,33 @@ function Location(nameoflocation, min, max, avgCookieSale) {
     this.totalCookiesPerLoc = [];                               // cookies number per hour 
 }
 
+$('#fcookies').submit(function (event)
+{
+    event.preventDefault();
+    // alert('hiiiiiiiiiiiiiiiiiiiiiiiiii');
+    var nameofloc = event.target.nameofloc.value;
+    console.log('name ', nameofloc);
+    var maxCust = parseInt(event.target.maxCust.value);
+    console.log('max ', maxCust);
+    var minCust = parseInt(event.target.minCust.value);
+    console.log('min ', minCust);
+    var avgCookieSale = parseFloat(event.target.avgCookieSale.value);
+    console.log('avg ', avgCookieSale);
+
+    var newLoc = new Location(nameofloc, minCust, maxCust, avgCookieSale);
+    locationsNames.push(newLoc);
+    // newLoc.locRow();
+    console.log('locationsNames : ', locationsNames);
+    // newLoc.cookiesValues();         // calculate the cookies for the new object 
+
+    $('#total').remove();
+    newLoc.cookiesValues();
+    newLoc.locRow();
+    tableFooter();
+
+});
+
+
 // Show up the table by create the table tag 
 $('#cookieJQ').html('<table></table');
 
@@ -23,21 +50,24 @@ $('#cookieJQ').html('<table></table');
 
 var seattle = new Location('seattle', 23, 65, 6.3);
 // console.log(' seattle object : ', seattle);
-
+locationsNames.push(seattle);
 var tokyo = new Location('tokyo', 3, 24, 1.2);
 // console.log(' tokyo object : ', tokyo);
+locationsNames.push(tokyo);
 
 var dubai = new Location('dubai', 11, 38, 3.7);
 // console.log(' dubai object : ', dubai);
+locationsNames.push(dubai);
 
 var paris = new Location('paris', 20, 38, 2.3);
 // console.log(' paris object : ', paris);
+locationsNames.push(paris);
 
 var lima = new Location('lima', 2, 16, 4.6);
 // console.log(' Lima object : ', lima);
+locationsNames.push(lima);
 
 ///////////////////////// First coloum anf row in table ////////////
-var locationsNames = [seattle, tokyo, dubai, paris, lima];
 var hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
 
 // headerRow Function 
@@ -98,11 +128,13 @@ Location.prototype.cookiesValues = function () {
 // The table header 
 tableHeadRow();
 
+// Event listener by using JQuery
+
 for (var i = 0; i < locationsNames.length; i++) {
-    // console.log('this.locationsNames[i] : ', this.locationsNames[i]);
-    this.locationsNames[i].cookiesValues();
+    console.log('locationsNames[i] : ', locationsNames[i]);
+    locationsNames[i].cookiesValues();
     // The table Data
-    this.locationsNames[i].locRow();
+    locationsNames[i].locRow();
     // this.locationsNames[i].render();
 }
 
@@ -131,4 +163,6 @@ function tableFooter() {
 } // End of table footer function 
 
 tableFooter();
+
+
 
