ef30e50e2c85217d1cee68703794f71fbcc2ddea
Nsuliman <nsuliman88@gmail.com>
2020-01-12 05:50:40 +0200

    Lab-07 Done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/********************************* Constructor Functions ********************************/
+/////////////////////// Constructor Functions ///////////////
 function Locations(nameoflocation, min, max, avgCookieSale) {
 
     this.name = nameoflocation;
@@ -8,79 +8,134 @@ function Locations(nameoflocation, min, max, avgCookieSale) {
     this.minCust = min;
     this.avgCookieSale = avgCookieSale;
     this.totalCookies = 0;
+    this.genArray = [];  // cookies number per hour 
+
 }
-
-// console.log('Locations Constructor Function : ', Locations);
-
-/********************************* The Objects From Table   ********************************/
+///////////////////// The Objects //////////////////////////
 var seattle = new Locations('seattle', 23, 65, 6.3);
-// console.log(' seattle object : ', seattle);
+console.log(' seattle object : ', seattle);
 
 var tokyo = new Locations('tokyo', 3, 24, 1.2);
-// console.log(' tokyo object : ', tokyo);
+console.log(' tokyo object : ', tokyo);
 
 var dubai = new Locations('dubai', 11, 38, 3.7);
-// console.log(' dubai object : ', dubai);
+console.log(' dubai object : ', dubai);
 
 var paris = new Locations('paris', 20, 38, 2.3);
-// console.log(' paris object : ', paris);
+console.log(' paris object : ', paris);
 
 var lima = new Locations('lima', 2, 16, 4.6);
-// console.log(' Lima object : ', lima);
+console.log(' Lima object : ', lima);
 
-/********************************* Data needed to calculate the functions I need  ********************************/
+
+///////////////////////// First coloum anf row in table ////////////
+var locationsNames = [seattle, tokyo, dubai, paris, lima];
 var hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
-var totalCookiesOfLocations = 0;
+var totalCookiesOfLocations = [];
+////////////////// Calculate the Random number /////////////////////
+Locations.prototype.randomInRange = function () {
 
+    var range = this.maxCust - this.minCust;
+    //console.log('range',range);
+    var randNum = Math.ceil((Math.random() * range) + this.minCust);
 
-// A Prototype Function To generate a Random Number for each location / instance from the constructor function 
-Locations.prototype.randomRange = function () {
-    let range = this.maxCust - this.minCust;
-    let rand = Math.ceil((Math.random() * range) + this.minCust);
-    // console.log('rand : ', rand);
-    return rand;
-} // end of randomRange function 
+    return randNum;
+}
+//var na =seattle.randomInRange();
+//console.log('na',na);
 
-
-// Calculate the cookies for each location at spesific hour ,Show up the Output For Each Location in the same function
+//////////////////////////// Calculate cookies per hour //////////////
 Locations.prototype.cookiesValues = function () {
 
-    var contArea = document.getElementById('showArea');
-    var header = document.createElement('h1');
-    header.textContent = this.name;
-    contArea.appendChild(header);
-    var ul = document.createElement('ul');
-    contArea.appendChild(ul);
+    for (var i = 0; i < 14; i++) {
+        var SeattleRNC = this.randomInRange(this.minCust, this.maxCust);
+        //console.log( ' Seattle RNC' , SeattleRNC);
+        var AmountOfCookieS = SeattleRNC * (this.avgCookieSale);
+        var output = Math.ceil(AmountOfCookieS);
+        //console.log( ' AmountOfCookieS ' , output);
+        this.genArray.push(output);
+        this.totalCookies = this.totalCookies + output;
+    }
+}
+//console.log('ff',this.genArray);
 
-    for (var i = 0; i<hours.length;i++)  
-    {   
-        var locRN = this.randomRange();
-        var numOfCookies = Math.ceil(locRN * (this.avgCookieSale));
-        // // console.log('numOfCookies : ', numOfCookies);
-        this.totalCookies += numOfCookies;
-        // // console.log('this.totalCookies : ', this.totalCookies);
-        var li = document.createElement('li');
-        li.textContent = hours[i] + ': ' + numOfCookies + ' cookies' ;
-        // console.log('li.textContent : ', li.textContent);
-        ul.appendChild(li);
-    } // end of ForLoop  
-
-    var pargh = document.createElement('p');
-    pargh.textContent =  this.name +' Total : ' + this.totalCookies + ' cookies' ;
-    ul.appendChild(pargh);
-
-    totalCookiesOfLocations += this.totalCookies;
-} // end of cookiesValues function 
-
-/********************************* Array of objects , calculate the hourly cookies for each object   ********************************/
-var locationsNames = [seattle, tokyo, dubai, paris, lima];
-
+//////////////calculate the hourly cookies for each object ///////////
 for (var i = 0; i < locationsNames.length; i++) {
     this.locationsNames[i].cookiesValues();
 }
 
-var contArea = document.getElementById('showArea');
-var section = document.createElement('section');
-section.textContent = 'The Total Numbers of Cookies For All Locations : ' + totalCookiesOfLocations;
-contArea.appendChild(section);
+////////////////////////////////// Show up the Table ///////////////
+
+
+var contArea = document.getElementById('showArea'); // Choose the place i would like to show my table inside it 
+
+var table = document.createElement('table');   // tell the broswer we need to create a table & push it 
+contArea.appendChild(table);
+
+
+var tabRow = document.createElement('tr');  // we need to put the table row  
+table.appendChild(tabRow);
+
+var HeadTab = document.createElement('th'); /// put the heading of the table 
+tabRow.appendChild(HeadTab);
+
+HeadTab.textContent = ' ';  /// to make a space above the table  
+
+for (var i = 0; i < hours.length; i++)                        /// insert the hours 
+{
+    var rowheadfirst = document.createElement('th');
+    tabRow.appendChild(rowheadfirst);
+    rowheadfirst.textContent = hours[i] ;
+}
+
+var totalCookiesLoc = document.createElement('th');  /// put the last header coloum in the table 
+tabRow.appendChild(totalCookiesLoc);
+totalCookiesLoc.textContent = ' Daily Location Total ';
+
+Locations.prototype.render = function () {
+    var rowData = document.createElement('tr');
+    table.appendChild(rowData);
+
+    var locNames = document.createElement('th');            /// insert the objects names 
+    rowData.appendChild(locNames);
+    locNames.textContent = this.name;
+
+
+    for (var i = 0; i < this.genArray.length; i++)          /// insert the cookies values
+    {
+        var cookieHourly = document.createElement('td');
+        rowData.appendChild(cookieHourly);
+        cookieHourly.textContent = this.genArray[i];
+    }
+    var total = document.createElement('td');
+    rowData.appendChild(total);
+    total.textContent = this.totalCookies;
+};
+
+for (var i = 0; i < locationsNames.length; i++) {
+    this.locationsNames[i].render();
+}
+
+var rowData = document.createElement('tr');
+    table.appendChild(rowData);
+    var total = document.createElement('td');
+    rowData.appendChild(total);
+    total.textContent = 'Totals';
+    for (var i = 0; i <=13; i++) {
+        var total = document.createElement('td');
+        rowData.appendChild(total);
+
+total.textContent = seattle.genArray[i]+tokyo.genArray[i]+dubai.genArray[i]+paris.genArray[i]+lima.genArray[i];
+    }
+    var total = document.createElement('td');
+    rowData.appendChild(total);
+
+total.textContent =seattle.totalCookies+tokyo.totalCookies+dubai.totalCookies+paris.totalCookies+lima.totalCookies;
+
+
+
+
+
+
+
 
